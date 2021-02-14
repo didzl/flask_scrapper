4789ab3870bc39d9171bb01fd4c361d619d5f70b
didzl <delpiero1987@naver.com>
2021-02-14 12:44:56 +0000

    stack overflow scrapper

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 flask with python in repl
 
 
+how to use in personal
+    Mac
+    1. 가상환경 만들기 => python3 -m venv venv
+    2. 가상환경 활성화하기 => . venv/bin/activate
+    3. Flask 설치하기 => pip install Flask
 
-Mac
-1. 가상환경 만들기 => python3 -m venv venv
-2. 가상환경 활성화하기 => . venv/bin/activate
-3. Flask 설치하기 => pip install Flask
+    Window
+    1. 가상환경 만들기 => py -3 -m venv venv
+    2. 가상환경 활성화하기 => venv\Scripts\activate
+    3. Flask 설치하기 => pip install Flask
 
-Window
-1. 가상환경 만들기 => py -3 -m venv venv
-2. 가상환경 활성화하기 => venv\Scripts\activate
-3. Flask 설치하기 => pip install Flask
+    여기까지 설치 다 하셨으면 main.py 파일 만드시고 저장하신 뒤에 다시 VSCODE 터미널에서 python3 main.py 실행하시면 서버가 실행됩니다!
 
-여기까지 설치 다 하셨으면 니꼬처럼 main.py 파일 만드시고 저장하신 뒤에 다시 VSCODE 터미널에서 python3 main.py 실행하시면 서버가 실행됩니다!
-
-* 당연하지만, VSCODE는 파이썬 환경이여야 해요~
-* 참고 사이트 : https://flask.palletsprojects.com/en/1.1.x/installation/#install-virtualenv
+    * 당연하지만, VSCODE는 파이썬 환경이여야 해요~
+    * 참고 사이트 : https://flask.palletsprojects.com/en/1.1.x/installation/#install-virtualenv
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
+from stack_scrapper import get_jobs
+from export import save_file
+
 
 app = Flask("han's Scrapper")
+
+# fake db
+db = {}
 
 @app.route("/") # only read function
 def home():
@@ -20,13 +26,44 @@ def report():
   if word:
     # lowcase in alphabet
     word = word.lower()
+    fromDB = db.get(word)
+    if fromDB:
+      jobs = fromDB
+    else:
+      jobs = get_jobs(word)
+      db[word] = jobs
 
   else:
     return redirect("/")
   # use {{searchby}} in template
   print(word)
-  return render_template("report.html", searchby = word) 
+  return render_template(
+    "report.html"
+    , searchby = word
+    , resultsNumber = len(jobs)
+    , jobs = jobs
+    ) 
 
 
+@app.route("/export")
+def export():
+  try:
+    word = request.args.get('word')
+    if not word:
+      raise Exception()
+      word = word.lower()
+    jobs = db.get(word)
+
+    if not jobs:
+      raise Exception()
+
+    save_file(jobs)
+
+    return send_file("jobs.csv")
+
+
+  except:
+    return redirect("/")
+  
 
 app.run(host="0.0.0.0") # 0.0.0.0 is repl's host
